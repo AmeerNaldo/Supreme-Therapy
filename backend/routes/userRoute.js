f81505bfef924fddeec7f5cd13d9cc8d940c4374
Ameer <ameerabdrabo7@gmail.com>
2024-10-03 03:00:37 -0500

    appointment api and user api

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { getProfile, loginUser, registerUser } from "../controllers/userController.js";
+import { bookAppointment, cancelAppointment, getProfile, listAppointment, loginUser, registerUser, updateProfile } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router()
 
@@ -8,6 +9,9 @@ userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
 
 userRouter.get('/get-profile', authUser, getProfile)
-
+userRouter.post('/update-profile', upload.single('image'), authUser, updateProfile)
+userRouter.post('/book-appointment', authUser, bookAppointment)
+userRouter.get('/appointments', authUser, listAppointment)
+userRouter.post('/cancel-appointment', authUser, cancelAppointment)
 
 export default userRouter
